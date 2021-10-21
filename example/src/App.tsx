a85cc4fb6803516ad26279ce09a354e28ce5abec
Aroldo Goulart <aroldogooulart@gmail.com>
2021-10-21 10:39:12 -0400

    Fix update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,60 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-screenshot-prevention';
+import { allowScreenCapture, preventScreenCapture, addScreenshotListener } from 'react-native-screenshot-prevention';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [screenshotEnabled, setScreenshotEnabled] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+    // Function to execute when user did a screenshot(ios only)
+    const onScreenshot = () => {
+      console.log("Hey, screenshot detected!")
+    }
+
+    // Its important have and "unsubcribe" to remove listener from screen is dismonted
+    const unsubscribe = addScreenshotListener(onScreenshot);
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+
+  const onChange = (nextStage: boolean): void => {
+    setScreenshotEnabled(nextStage)
+    if (nextStage) {
+      allowScreenCapture()
+    }
+    else {
+      preventScreenCapture()
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+
+      <TouchableOpacity
+        onPress={() => onChange(true)}
+        style={[styles.touchable, { backgroundColor: '#6eff61' }]}
+      >
+        <Text>
+          Enable
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => onChange(false)}
+        style={[styles.touchable, { backgroundColor: '#d9326c' }]}
+      >
+        <Text>
+          Disable
+        </Text>
+      </TouchableOpacity>
+
+      <Text>
+        Screenshot enabled: {screenshotEnabled}
+      </Text>
     </View>
   );
 }
@@ -23,9 +65,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  touchable: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    marginHorizontal: 10
   },
 });
